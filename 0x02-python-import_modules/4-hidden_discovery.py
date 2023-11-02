6a1e2fb4f667eaa9269696da98edaf1c35f44af7
Joe-0009 <youssefrachidi1994@gmail.com>
2023-11-02 04:58:41 -0700

    add comments

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,8 @@
 
 if __name__ == "__main__":
     import hidden_4
-    
+
+    names = dir(hidden_4)
     for name in names:
         if name[:2] != "__":
-            print(dir(hidden_4))
+            print(name)
