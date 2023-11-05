f8ef834646ca69dc60c063ca0bd5d0f1399ccf9e
Joe-0009 <youssefrachidi1994@gmail.com>
2023-11-05 05:17:49 -0800

    gg

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-
-def divisible_by_2(my_list=[]):
-    new_l = my_list.copy()
-    for i in range(0, len(my_list)):
-        if my_list[i] % 2 == 0:
-            new_l[i] = 1
-        else:
-            new_l[i] = 0
-    return new_l
+a = 89
+b = 10
+a, b = b, a
+print("a={:d} - b={:d}".format(a, b))
