a41deafe4430cac1fd8395713a7224dfda848f3c
Joe-0009 <youssefrachidi1994@gmail.com>
2023-11-08 02:27:31 -0800

    keep learning

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    for i in my_list:
-        if i == search:
-            i = replace
+    lis = my_list.copy()
+    for i in(range(len(lis))):
+        if lis[i] == search:
+            lis[i] = replace
+    return lis
