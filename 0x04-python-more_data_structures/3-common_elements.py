4504d0e407c1e3212e9fa961ceb9698888743ed7
Joe-0009 <youssefrachidi1994@gmail.com>
2023-11-08 02:48:44 -0800

    keep learning

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-
 def common_elements(set_1, set_2):
-    # The & operator is used to find the intersection of two sets
     common_set = set_1 & set_2
-    
     return common_set
-
