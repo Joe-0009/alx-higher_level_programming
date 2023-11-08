bae7310b4493d2e4b81209a83161d3094786386b
Joe-0009 <youssefrachidi1994@gmail.com>
2023-11-08 14:20:30 -0800

    gg

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    # Check if the input is a string
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    # Define the Roman numerals and their integer value
+    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    # Initialize the previous value and total
+    prev_value = 0
+    total = 0
+
+    # Iterate over each character in the Roman numeral string
+    for numeral in reversed(roman_string):
+        # Convert the Roman numeral to an integer
+        value = roman_numerals.get(numeral, 0)
+
+        # If the numeral is smaller than the previous one, subtract it
+        if value < prev_value:
+            total -= value
+        else:
+            # Otherwise, add it and update the previous value
+            total += value
+            prev_value = value
+
+    return total
