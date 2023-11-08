f835c0c60421f80fd001d6f2cacb3c71835470ef
Joe-0009 <youssefrachidi1994@gmail.com>
2023-11-08 12:21:17 -0800

    commit

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
 def best_score(a_dictionary):
-    max_ = 0
-    for score in a_dictionary:
-        if score > max_:
-            max_ = score
-    return a_disctionary[max_]
+    if not a_dictionary:
+        return (None)
+
+    return (max(a_dictionary, key=a_dictionary.get))
