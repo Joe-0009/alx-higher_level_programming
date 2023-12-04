057e2dde7eb52736da38fd7fecad38a826a02f7a
Joe-0009 <youssefrachidi1994@gmail.com>
2023-12-04 14:00:06 -0800

    empty class with first fucntion

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""defines a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """represent a BaseGeometry class"""
+
+    def area(self):
+        """represent Area """
+        raise Exception("area() is not implemented")
