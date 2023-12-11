cb55bd1be3c3ed7d6d02d58ebac3531d15a6e4c4
Joe-0009 <youssefrachidi1994@gmail.com>
2023-12-11 00:54:58 -0800

    base update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """ Module that contains class Base """
+import json
+import csv
+import turtle
 
 
 class Base:
@@ -13,3 +16,14 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+
+        return json.dumps(list_dictionaries)
+
+    def save_to_file(cls, list_objs):
+        """Write the JSON serialization of a list of objects to a file."""
+        
