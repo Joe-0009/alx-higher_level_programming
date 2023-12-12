cc62791079fa9851f65337c73d033f3ad6e767a0
Joe-0009 <youssefrachidi1994@gmail.com>
2023-12-12 12:36:37 -0800

    gg

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -15,3 +15,18 @@ class TestBaseMethods(unittest.TestCase):
     def setUp(self):
         """ Method invoked for each test """
         Base._Base__nb_objects = 0
+
+    def test_three_bases(self):
+        b1 = Base()
+        b2 = Base()
+        b3 = Base()
+        self.assertEqual(b1.id, b3.id - 2)
+
+    def test_nb_instances_after_unique_id(self):
+        b1 = Base()
+        b2 = Base(12)
+        b3 = Base()
+        self.assertEqual(b1.id, b3.id - 1)
+
+    def test_dict_id(self):
+        self.assertEqual({"a": 1, "b": 2}, Base({"a": 1, "b": 2}).id)
