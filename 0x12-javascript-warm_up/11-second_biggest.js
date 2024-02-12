b67c5659e156ba3affdbce95fd14ab891b92bef0
Joe-0009 <youssefrachidi1994@gmail.com>
2024-02-12 21:45:19 +0000

    11th task

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const numbers = process.argv.slice(2).map(Number);
+
+numbers.sort((a, b) => b - a);
+
+if (numbers.length <= 1) {
+    console.log(0);
+} else {
+    console.log(numbers[1]);
+}
