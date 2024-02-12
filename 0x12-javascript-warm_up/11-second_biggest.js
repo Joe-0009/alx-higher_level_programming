98494ac547ebbaaa4b6851516fb51683b2e69b3d
Joe-0009 <youssefrachidi1994@gmail.com>
2024-02-12 21:52:24 +0000

    11th task

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,12 @@
-#!/usr/bin/node
+#!/usr/bin/node;
 
+// Extract command-line arguments starting from index 2 and convert them to integers
 const numbers = process.argv.slice(2).map(Number);
 
+// Sort the numbers in descending order
 numbers.sort((a, b) => b - a);
 
-if (numbers.length <= 3) {
+if (numbers.length <= 1) {
     console.log(0);
 } else {
     console.log(numbers[1]);
