9dfb6c1a391c51155296328a588c130b1e4370b9
Joe-0009 <youssefrachidi1994@gmail.com>
2024-02-12 17:21:26 +0000

    7th task

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const str = 'C is fun\nPython is cool\nJavaScript is amazing';
+const str = 'C is fun\nPython is cool\nJavaScript is amazing\n';
 
 for (let i = 0; i < str.lenght; i++) {
   console.log(str[i]);
