3573d596a93aca8dd78f364927749e314aa32842
Joe-0009 <youssefrachidi1994@gmail.com>
2024-02-12 13:00:03 +0000

    third task

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-if (console.argv.length == 2){
+if (console.argv.length == 2) {
 	console.log('No argument');
-}else if (console.argv.length == 3){
+} else if (console.argv.length == 3) {
 	console.log('Argument found');
-}else{
+} else {
 	console.log('Arguments found');
 }
