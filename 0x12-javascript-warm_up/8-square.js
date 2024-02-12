4808b0ef8eeea4e5efc695698e75f58141359497
Joe-0009 <youssefrachidi1994@gmail.com>
2024-02-12 21:09:58 +0000

    8th task

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,6 +3,8 @@ if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < Number(process.argv[2]); i++) {
-    console.log('X');
+    for (let j = 0; j < Number(process.argv[2]); i++) {
+      console.log('X');
+    }
   }
 }
