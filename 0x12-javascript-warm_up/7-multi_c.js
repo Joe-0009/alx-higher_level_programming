4658e125e42d6e940433982233b890a359095fe2
Joe-0009 <youssefrachidi1994@gmail.com>
2024-02-12 19:27:27 +0000

    8th task

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,6 +2,8 @@
 if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
   cosole.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < Number(process.argv[2]); i++) {
+  let i = 0;
+  while (i < Number(process.argv[2]) {
     console.log('C is fun');
+    i++;
 }
