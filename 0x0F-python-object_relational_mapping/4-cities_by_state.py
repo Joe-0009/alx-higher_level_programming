22718592d02ce3de6d06b4b594750d3573800c59
Joe-0009 <youssefrachidi1994@gmail.com>
2024-02-17 03:17:49 -0800

    4th task

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,17 +1,40 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
+"""
+Script that lists all cities from the database hbtn_0e_4_usa
+"""
+
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("""SELECT cities.id, cities.name, states.name FROM
-                cities INNER JOIN states ON states.id=cities.state_id""")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
+    # Database connection parameters
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    # Connect to MySQL server running on localhost at port 3306
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=db_name, charset="utf8")
+
+    # Create a cursor object using cursor() method
+    cursor = db.cursor()
+
+    # Execute SQL query to select cities from the cities table and join with the states table to get state names
+    query = "SELECT cities.id, cities.name, states.name " \
+            "FROM cities " \
+            "JOIN states ON cities.state_id = states.id " \
+            "ORDER BY cities.id ASC"
+
+    cursor.execute(query)
+
+    # Fetch all the rows in a list of tuples
+    cities = cursor.fetchall()
+
+    # Print the cities
+    for city in cities:
+        print(city)
+
+    # Close the cursor and database connection
+    cursor.close()
     db.close()
+
