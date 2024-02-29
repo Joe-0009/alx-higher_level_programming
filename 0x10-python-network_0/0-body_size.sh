d30d42c6606660ccaedfbaf79306e0a9edc4f7bb
Joe-0009 <youssefrachidi1994@gmail.com>
2024-02-29 05:23:25 -0800

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
-
-# Send a request to the specified URL and display the size of the response body in bytes
-
-# Usage: ./0-body_size.sh <URL>
-
-# Make sure the URL is provided as an argument
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Extract the size of the response body using curl
-SIZE=$(curl -sI "$1" | grep -i 'content-length' | awk '{print $2}')
-
-# Check if the size is present
-if [ -z "$SIZE" ]; then
-    echo "Could not determine the size of the response body."
-else
-    echo "$SIZE"
-fi
+# Sends a request to that URL, and displays the size of the body of the response
+curl -s "${1}" | wc -c
